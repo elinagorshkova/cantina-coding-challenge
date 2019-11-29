da06b28b032ce88ef75449b0ddb244e12a4d72b0
elinag <elina.gorshkova@icloud.com>
2019-11-29 14:51:02 -0500

    Add starting question
    Boolean question for inquirer: proceed: yes/no

--- assets/questions.js
+++ assets/questions.js
@@ -1,6 +1,13 @@
 'use strict'
 // THE LIST OF QUESTIONS TO BE USED BY THE INQUIRER NODE MODULE
 
+// Start and confirm the json source url
+const begin = {
+    type: 'confirm',
+    name: 'source',
+    message: '\n🙌 Welcome to my program!\nProceed?'
+  }
+
 module.exports = {
-  
+  begin
 }
