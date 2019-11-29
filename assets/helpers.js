ed8dec4a7b73785c01b359837f77fe43ce24e354
elinag <elina.gorshkova@icloud.com>
2019-11-29 15:53:02 -0500

    Add searching function
    The function iterates through a JSON file and searches for all values
    for a given key.
    Returns an array of values

--- assets/helpers.js
+++ assets/helpers.js
@@ -11,6 +11,34 @@ const fetchingData = (url) => {
     })
 }
 
+const searchingFunction = (jsonFile, lookupKey) => {
+  // Declaring and empty array that we will populate with all the matches
+  let values = []
+  for (const i in jsonFile) {
+      // Skipping the parts of the JSON that are not objects (don`t have a key)
+        if (!jsonFile.hasOwnProperty(i)) continue
+        // If key of the current object matches our key...
+        if (i === lookupKey) {
+        // ... if the value of the key is a string...
+        if (typeof jsonFile[i] == 'string') {
+          // ... push the string in the objects array
+          values.push(jsonFile[i])
+        } else {
+          // Othervise, the value of the key must be an array, so we iterate though the array,
+          // and push each value in the objects array
+          jsonFile[i].forEach(value => values.push(value))
+        }
+        // Going deeper in the JSON object
+        // Searching for the key instances in the 'children' objects of the JSON
+      } else if (typeof jsonFile[i] === 'object') {
+          values = values.concat(searchingFunction(jsonFile[i], lookupKey));
+        }
+    }
+    // Returning the array of the values
+    return values
+}
+
 module.exports = {
-  fetchingData
+  fetchingData,
+  searchingFunction
 }
