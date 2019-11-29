a6ae8335abe38859402afdd5257b0d1f11f4a170
elinag <elina.gorshkova@icloud.com>
2019-11-29 16:02:48 -0500

    Add store.js
    Will use the file to store some data and make it available across all the files

---
+++ store.js
@@ -0,0 +1,6 @@
+const store = {
+  url: 'https://raw.githubusercontent.com/jdolan/quetoo/master/src/cgame/default/ui/settings/SystemViewController.json',
+  instances: [],
+}
+
+module.exports = store
