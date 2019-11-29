ac89f18e44d9b8cc9a2812e5338ac7391dff9b73
elinag <elina.gorshkova@icloud.com>
2019-11-29 15:47:24 -0500

    Add tests for searchingFunction
    Used chai assert for write the tests.
    Two conditions: the function should return array, the array should have
    certain values (from dummyJson file)

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,9 +1,31 @@
 const assert = require('chai').assert
-const chai = require('chai')
+const { searchingFunction } = require('./../assets/helpers')
 
 // WE NEED A FUNCTION THAT WOULD PARSE A JSON FILE AND WOULD SEARCH THROUGH THE FILE FOR ALL THE VALUES THAT MATCH A CERTAIN KEY
 // a) need a dummy json:
-
+const dummyJSON = {
+  dummyClass: 'Dummy1',
+  subClass:
+    [{
+      dummyClass: 'dummy2',
+      someKey: 'none'
+    },
+    {subSubClass: {
+      dummyClass: 'DUMMY3'
+    }
+  }]
+}
 // b) need conditions:
 // - output should be an array
 // - with the particular dummy JSON output should be ['Dummy1', 'dummy2', 'DUMMY3']
+describe('App', function() {
+  it(`should be an array`, function() {
+    const result = searchingFunction(dummyJSON, 'dummyClass')
+    assert.typeOf(result, 'array')
+  })
+
+  it(`should return ['Dummy1', 'dummy2', 'DUMMY3']`, function() {
+    const result = searchingFunction(dummyJSON, 'dummyClass')
+    assert.deepEqual(result, ['Dummy1', 'dummy2', 'DUMMY3'])
+  })
+})
