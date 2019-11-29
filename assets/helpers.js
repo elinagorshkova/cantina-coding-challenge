6b517affc3c516c946107041aa72384fa19f4344
elinag <elina.gorshkova@icloud.com>
2019-11-29 16:06:28 -0500

    Add findEveryInstance function
    The function does two things:
    a) returns a unique array of values that matched a certain key
    b) counts number of occurances for every value and stores it

--- assets/helpers.js
+++ assets/helpers.js
@@ -2,6 +2,12 @@
 // Promise based HTTP client for Node.js
 const axios = require('axios')
 
+// Node module, that counts the instances of each value in an array, ignoring any non-string values.
+const count = require('count-array-values')
+
+// Using Store file to store data for further use in it
+const store = require('./../store')
+
 const fetchingData = (url) => {
   // Fetching the JSON file from the URL
    axios.get(url)
@@ -38,7 +44,27 @@ const searchingFunction = (jsonFile, lookupKey) => {
     return values
 }
 
+const findEveryInstance = (valuesArray) => {
+  // Using the 'count-array-values' module to find every instance of the picked selector and count how many occurances of the instance there are
+//Returns an array of objects: [
+  // {value; 'Input', count: 5}
+  // {value: 'something', count: 4}
+// ]
+const instances = count(valuesArray)
+// Storing the counted object in the store
+store.instances = instances
+// Mapping through the array to get only the values from the object
+const eachValue = instances.map((instance) => {
+  return instance.value
+})
+store.eachValue = eachValue
+// Returning an array with every matching value for the picked attribute:
+// User picked Class - return ['Input', 'CvarSlider', 'CvarSelect', 'StackView', 'Box', 'CvarCheckbox', 'VideoModeSelect' ]
+return eachValue
+}
+
 module.exports = {
   fetchingData,
-  searchingFunction
+  searchingFunction,
+  findEveryInstance
 }
