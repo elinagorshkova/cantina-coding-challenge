fe5b999c1e62e4257776f0642e3f974f85d22fd2
elinag <elina.gorshkova@icloud.com>
2019-11-29 15:44:20 -0500

    Add basic user stories to start writing tests

---
+++ test/indexTest.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert
+const chai = require('chai')
+
+// WE NEED A FUNCTION THAT WOULD PARSE A JSON FILE AND WOULD SEARCH THROUGH THE FILE FOR ALL THE VALUES THAT MATCH A CERTAIN KEY
+// a) need a dummy json:
+
+// b) need conditions:
+// - output should be an array
+// - with the particular dummy JSON output should be ['Dummy1', 'dummy2', 'DUMMY3']
